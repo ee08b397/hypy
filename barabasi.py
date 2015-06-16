3972befbb6b130e5731191e73ba90ab86f30346b
Songxiao Zhang <songxiaozhang@S.local>
2015-06-16 16:00:13 -0400

    non-OO construct_tree works in barabasi.py

--- barabasi.py
+++ barabasi.py
@@ -1,37 +1,28 @@
 import igraph
+import collections
+import json
 
 def bas():
 	g = igraph.Graph.Barabasi(n=10, m=2)
-	# g.spanning_tree(None, True).get_edgelist()
-	# print g.get_edgelist()
-	# return g.get_edgelist()
 	return g.spanning_tree(None, True).get_edgelist()
 
-# bas()
-
-import collections
-import json
 #http://xahlee.info/perl-python/python_construct_tree_from_edge.html
-
-def construct_trees(edges):
+def construct_tree(edges, root):
 	"""Given a list of edges [child, parent], return trees. """
 	trees = collections.defaultdict(dict)
 
-	for child, parent in edges:
+	for parent,child in edges:
 			trees[parent][child] = trees[child]
 
-	# Find roots
 	children, parents = zip(*edges)
-	roots = set(parents).difference(children)
-
-	return {root: trees[root] for root in roots}
+	return {root: trees[root]}
 
 if __name__ == '__main__':
 	# edges = [[0, 2], [3, 0], [1, 4], [2, 4], [5, 6], [6, 7], [8, 6]]
 	edges = bas()
-	print(json.dumps(construct_trees(edges), indent=2))
-	graph = construct_trees(edges)
-	print graph
+	print edges	
+	graph = construct_tree(edges, 0)
+	print(json.dumps(graph, indent=2))
 
 
 
